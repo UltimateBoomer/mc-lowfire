951706de1c094cf7bc8ec2ea5c32fa558bb54175
Steven Cao <caohc2017@gmail.com>
2021-01-05 15:23:45 -0500

    Add tooltip in config

--- src/main/java/io/github/ultimateboomer/lowfire/LowFireConfig.java
+++ src/main/java/io/github/ultimateboomer/lowfire/LowFireConfig.java
@@ -2,8 +2,10 @@ package io.github.ultimateboomer.lowfire;
 
 import me.sargunvohra.mcmods.autoconfig1u.ConfigData;
 import me.sargunvohra.mcmods.autoconfig1u.annotation.Config;
+import me.sargunvohra.mcmods.autoconfig1u.annotation.ConfigEntry;
 
 @Config(name = LowFire.MODID)
 public class LowFireConfig implements ConfigData {
+	@ConfigEntry.Gui.Tooltip
 	public double fireOffset = 0.3;
 }
--- src/main/java/io/github/ultimateboomer/lowfire/mixin/InGameOverlayRendererMixin.java
+++ src/main/java/io/github/ultimateboomer/lowfire/mixin/InGameOverlayRendererMixin.java
@@ -1,14 +1,12 @@
 package io.github.ultimateboomer.lowfire.mixin;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import io.github.ultimateboomer.lowfire.LowFire;
+import net.minecraft.client.gui.hud.InGameOverlayRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import com.mojang.blaze3d.systems.RenderSystem;
-
-import net.minecraft.client.gui.hud.InGameOverlayRenderer;
 
 @Mixin(InGameOverlayRenderer.class)
 public class InGameOverlayRendererMixin {
