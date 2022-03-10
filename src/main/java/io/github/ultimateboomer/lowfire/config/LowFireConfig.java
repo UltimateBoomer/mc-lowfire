18b3cc89d82ba7726b9dcf7ce6bb8a5d6ca7a9c3
dhyces <pokmon987@gmail.com>
2022-03-09 23:57:31 -0500

    Remove unused translation, change comment, and remove reload event listener

--- src/main/java/io/github/ultimateboomer/lowfire/config/LowFireConfig.java
+++ src/main/java/io/github/ultimateboomer/lowfire/config/LowFireConfig.java
@@ -2,15 +2,9 @@ package io.github.ultimateboomer.lowfire.config;
 
 import java.util.List;
 
-import io.github.ultimateboomer.lowfire.LowFire;
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.common.ForgeConfigSpec.DoubleValue;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
-import net.minecraftforge.fml.event.config.ModConfigEvent;
 
-@EventBusSubscriber(modid = LowFire.MOD_ID, bus = Bus.MOD, value = Dist.CLIENT)
 public class LowFireConfig {
 
 	public static class Client {
@@ -19,8 +13,7 @@ public class LowFireConfig {
 		public Client(ForgeConfigSpec.Builder builder) {
 			builder.push("Client");
 
-			fireOffset = builder.comment("Changes the offset of the fire effect. Positive values move it down, negative move up.")
-								.translation("lowfire.")
+			fireOffset = builder.comment("Changes the offset of the fire effect")
 								.defineInRange(List.of("FireOffset"), 0.3, 0.0, 0.5);
 		}
 	}
@@ -31,12 +24,5 @@ public class LowFireConfig {
 		var pair = new ForgeConfigSpec.Builder().configure(Client::new);
 		spec = pair.getRight();
 		client = pair.getLeft();
-	}
-
-	public static void reload(final ModConfigEvent.Reloading event) {
-		if (event.getConfig().getSpec() == spec) {
-			client.fireOffset.set(event.getConfig().getConfigData().getOrElse("FireOffset", 0.3));
-			System.out.println("RELOADING");
-		}
 	}
 }
