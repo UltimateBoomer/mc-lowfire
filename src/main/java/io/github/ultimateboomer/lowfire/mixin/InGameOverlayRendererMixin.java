025aa68beeb463f555202aff0c5b20d715867443
dhyces <pokmon987@gmail.com>
2022-03-10 17:55:36 -0500

    Rename method and give more info on the injection

--- src/main/java/io/github/ultimateboomer/lowfire/mixin/InGameOverlayRendererMixin.java
+++ src/main/java/io/github/ultimateboomer/lowfire/mixin/InGameOverlayRendererMixin.java
@@ -10,13 +10,16 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import io.github.ultimateboomer.lowfire.config.LowFireConfig;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.ScreenEffectRenderer;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 
+@OnlyIn(Dist.CLIENT)
 @Mixin(ScreenEffectRenderer.class)
 public class InGameOverlayRendererMixin {
-	@Inject(method = "renderFire",
+	@Inject(method = "renderFire(Lnet/minecraft/client/Minecraft;Lcom/mojang/blaze3d/vertex/PoseStack;)V",
 			at =@At(value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/PoseStack;translate(DDD)V"),
 			cancellable = true)
-	private static void renderFireOverlay(Minecraft client, PoseStack matrices, CallbackInfo ci) {
+	private static void renderFire(Minecraft client, PoseStack matrices, CallbackInfo ci) {
 		matrices.translate(0.0, -LowFireConfig.client.fireOffset.get(), 0.0);
 	}
 }
